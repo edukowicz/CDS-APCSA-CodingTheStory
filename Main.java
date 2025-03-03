4ba75326cc2d28a01fc260602e2f51fcdff4134e
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-03-03 19:45:32 +0000

    message

--- Main.java
+++ Main.java
@@ -4,20 +4,30 @@ class Main {
 
     //instantiate one object of each of the Pets
     Person Ada = new Person("Ada");
+    Pet Viola = new RubberDuck("Viola");
+    Pet Firefox = new Dog("Firefox");
+    Pet Chrome = new Cat("Chrome");
+    Pet Opera = new Fish("Opera");
 	
 
 
 
     //add all the Pets to Ada's petName list
+    Ada.addPet(Viola);
+    Ada.addPet(Firefox);
+    Ada.addPet(Chrome);
+    Ada.addPet(Opera);
 
 
 
 
     //call greetPets() for Ada
+    Ada.greetPets();
 
 
 
     //call feedPets() for Ada
+    Ada.feedPets();
 
 
   }
--- Person.java
+++ Person.java
@@ -14,16 +14,20 @@ public class Person {
 	// Complete the method below. You need to:
 	// 1. Insert the correct parameter.
 	// 2. Correctly add this object to petNames.
-	public void addPet(  ) {
-		
+	public void addPet(Pet p) {
+		petNames.add(p);
 	}
 
 
 	// Complete the method below. You need to:
 	// loop over petNames and print each Pet's name + ' has been fed.' Only Animals should be fed.
 	public void feedPets() {
-		
+		for(int i = 0; i < petNames.size(); i++){
+			if(petNames.get(i) instanceof Dog || petNames.get(i) instanceof Fish || petNames.get(i) instanceof Cat) {
+				System.out.print(petNames.get(i).getName() + " has been fed. ");
+			}
 		}
+		System.out.println();
 	}
 
 
@@ -31,7 +35,20 @@ public class Person {
 	// loop over petNames and print 'Hello ' + each Pet's name.
 	// Each Pet that can speak should then speak to Ada.
 	public void greetPets() {
-		
+		for(int i = 0; i < petNames.size(); i++){
+			if(petNames.get(i) instanceof Dog) {
+				System.out.println("Hello " + petNames.get(i).getName() + ". ");
+				((Dog)petNames.get(i)).speak();
+			}
+			if(petNames.get(i) instanceof Cat) {
+				System.out.println("Hello " + petNames.get(i).getName() + ". ");
+				((Cat)petNames.get(i)).speak();
+			}
+			if(petNames.get(i) instanceof Fish) {
+				System.out.println("Hello " + petNames.get(i).getName() + ". ");
+				((Fish)petNames.get(i)).speak();
+			}
+		}
 	}
 
 }
